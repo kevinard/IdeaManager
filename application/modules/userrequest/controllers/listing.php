b10a7acb6db132123b7d3ee7a3288540ed3e8f8e
Kevin O'NEILL <contact@kevinoneill.fr>
2011-08-19 13:03:36 +0200

    modif userrequest

--- application/modules/userrequest/controllers/listing.php
+++ application/modules/userrequest/controllers/listing.php
@@ -3,9 +3,18 @@ namespace application\modules\userrequest\controllers;
 
 class Listing extends \framework\core\Controller
 {
-    public function processAction($state = null)
+    public function processAction($catId = null, $state = null)
     {
-        $userrequests = $this->getComponent('entityManager')->getRepository('application\modules\userrequest\models\UserRequest')->findBy(array('state' => $state));
+        if ($catId === null)
+		{
+			$catId = '*';
+		}
+		if ($state === null)
+		{
+			$state = '*';
+		}
+		
+		$userrequests = $this->getComponent('entityManager')->getRepository('application\modules\userrequest\models\UserRequest')->findBy(array('category' => $catId, 'state' => $state));
 
         $this->set('userrequests', $userrequests);
     }
